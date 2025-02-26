73041a0f7e66f3e5139692480ade85d0cb5da319
VirtuMaks <Virtumaks49@gmail.com>
2025-02-26 02:05:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$ (function(){
+   $('.bike-slider, .slider__bisycle').slick({
+      arrows: false,
+      dots: true,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+   });
+});
